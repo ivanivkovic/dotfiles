ed7ab85642d83825126ef8d5922680340965361f
Ivan <ivan.ivkovichh@gmail.com>
2020-10-29 13:55:14 +0100

    Update

--- install.sh
+++ install.sh
@@ -9,3 +9,8 @@ echo 'Copy complete!'
 echo 'Applying changes to .bashrc...'
 cat .bashrc >> ~/.bashrc   
 echo 'Setup complete!'
+
+# Commands for setup
+# 
+# Terminal command
+# tmux new-session "tmux source-file ~/git/dotfiles/.tmux.conf"  
